55d110b03ac2b15ec7f0a1b918fb0a488d10ad7f
Lucas Santos <lucas.santos@pagtel.com.br>
2022-09-26 12:22:11 -0300

    fix: add mongodb connection to consumer

--- src/main/consumer.ts
+++ src/main/consumer.ts
@@ -1,9 +1,16 @@
 import { rabbitMqServer } from '@/infra/mq/utils';
 import { consumersSetup } from '@/main/configs/consumers';
-import { logger, RABBIT } from '@/util';
+import { logger, MONGO, RABBIT } from '@/util';
+import mongoose from 'mongoose';
 
 (async () => {
   try {
+    await mongoose.connect(MONGO.URL(), {
+      dbName: MONGO.NAME,
+      authSource: MONGO.AUTH_SOURCE,
+      authMechanism: 'SCRAM-SHA-1',
+    });
+
     const server = rabbitMqServer();
 
     server.setCredentials({
