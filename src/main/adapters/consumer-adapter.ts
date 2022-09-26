a35be125e0d8bd3c74162f5a2a9632bc05904a17
Lucas Santos <lucas.santos@pagtel.com.br>
2022-09-26 12:21:19 -0300

    fix: fix scope issue in state

--- src/main/adapters/consumer-adapter.ts
+++ src/main/adapters/consumer-adapter.ts
@@ -2,25 +2,30 @@ import { Job } from '@/consumer/protocols';
 
 import makeFlow from './fow-adapter';
 
+const STATE_KEY = Symbol('STATE');
+
 export const consumerAdapter = (...jobs: Job[]) => {
   type State = { [key: string]: any };
 
-  const state = {} as State;
-
-  const setState = (data: State) => {
-    for (const key in data) {
-      if (typeof key === 'string' || typeof key === 'number')
-        state[key] = data[key];
-    }
-  };
-
   const adaptedJobs = jobs.map((job) => {
-    return (payload: Job.Payload, next: Job.Next) => {
-      return job.handle(payload, [state, setState], next);
+    return (
+      payload: Job.Payload & { [key: string | symbol]: any },
+      next: Job.Next
+    ) => {
+      const setState = (data: State) => {
+        for (const key in data) {
+          if (typeof key === 'string' || typeof key === 'number')
+            payload[STATE_KEY][key] = data[key];
+        }
+      };
+
+      const stateHook = <[any, any]>[payload[STATE_KEY], setState];
+
+      return job.handle(payload, stateHook, next);
     };
   });
 
   return (payload: object) => {
-    return makeFlow(payload)(...adaptedJobs)();
+    return makeFlow({ ...payload, [STATE_KEY]: {} })(...adaptedJobs)();
   };
 };
