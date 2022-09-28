1c4a84b1b7ee505b8a951b1a2651cf2573b1b700
Lucas Santos <lucas.santos@pagtel.com.br>
2022-09-27 22:43:16 -0300

    chore: fix Mongodb default date

--- src/infra/db/mongodb/input-and-output-log/input-and-output-log-model.ts
+++ src/infra/db/mongodb/input-and-output-log/input-and-output-log-model.ts
@@ -10,7 +10,7 @@ const schema = new mongoose.Schema(
   {
     __v: { select: false },
     type: { type: String },
-    created_at: { type: Date, default: new Date() },
+    created_at: { type: Date, default: () => new Date() },
   },
   { strict: false }
 );
--- src/infra/db/mongodb/log/log-model.ts
+++ src/infra/db/mongodb/log/log-model.ts
@@ -11,7 +11,7 @@ const schema = new mongoose.Schema(
   {
     __v: { select: false },
     level: { type: String },
-    created_at: { type: Date, default: new Date() },
+    created_at: { type: Date, default: () => new Date() },
   },
   { strict: false }
 );
