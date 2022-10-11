e5a8bad01a86d5908f1a5cbbaf73f2f7af31fe8b
Lucas Santos <lucas.santos@pagtel.com.br>
2022-10-11 10:53:06 -0300

    fix: fix state scope

--- src/main/adapters/task-adapter.ts
+++ src/main/adapters/task-adapter.ts
@@ -2,25 +2,32 @@ import { Task } from '@/schedule/protocols';
 
 import makeFlow from './fow-adapter';
 
-export const taskAdapter = (...tasks: Task[]) => {
-  type State = { [key: string]: any };
+const STATE_KEY = Symbol('STATE');
 
-  const state: State = {};
-
-  const setState = (data: State) => {
-    for (const key in data) {
-      if (typeof key === 'string' || typeof key === 'number')
-        state[key] = data[key];
-    }
-  };
+export const taskAdapter = (...tasks: (Task | Function)[]) => {
+  type State = Record<string, unknown>;
 
   const adaptedTasks = tasks.map((task) => {
-    return (_: unknown, next: Task.Next) => {
-      return task.handle([state, setState], next);
+    return (
+      { [STATE_KEY]: state }: { [key: string | symbol]: State },
+      next: Task.Next
+    ) => {
+      const setState = (data: State) => {
+        for (const key in data) {
+          if (typeof key === 'string' || typeof key === 'number')
+            state[key] = data[key];
+        }
+      };
+
+      const stateHook = <[any, any]>[state, setState];
+
+      if (typeof task === 'function') return task(stateHook, next);
+
+      return task.handle(stateHook, next);
     };
   });
 
   return () => {
-    return makeFlow({})(...adaptedTasks)();
+    return makeFlow({ [STATE_KEY]: {} })(...adaptedTasks)();
   };
 };
