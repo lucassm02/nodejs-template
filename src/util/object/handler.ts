b215516fb8ae23f9223e841bc8d03b5377b12201
Lucas Santos <lucas.santos@pagtel.com.br>
2022-10-13 15:46:57 -0300

    feat: add possibility to access arrays in objects

--- src/util/object/handler.ts
+++ src/util/object/handler.ts
@@ -1,9 +1,14 @@
 export const getIn = (object: Object, path: string) => {
   const splittedPath = path.split('.');
-  const targetValue = splittedPath.reduce(
-    (memo: any, key: string) => memo && memo[key] && memo[key],
-    object
-  );
+  const targetValue = splittedPath.reduce((memo: any, key: string) => {
+    const index = <number | null>key.match(/[(\d*)]/);
+
+    if (index) {
+      return memo && memo[index] && memo[index];
+    }
+
+    return memo && memo[key] && memo[key];
+  }, object);
 
   return targetValue;
 };
