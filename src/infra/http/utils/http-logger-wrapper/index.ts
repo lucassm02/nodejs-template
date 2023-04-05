4d88c2a32a58a317c7f51891199a62e235d10800
Lucas Santos <lucas.santos@pagtel.com.br>
2023-04-05 10:18:56 -0300

    refactor: add error handler

--- src/infra/http/utils/http-logger-wrapper/index.ts
+++ src/infra/http/utils/http-logger-wrapper/index.ts
@@ -15,39 +15,43 @@ export const httpLoggerWrapper = ({
   keywords,
   services,
 }: Options) => {
-  const requestTypeOf = typeof request?.body;
-  const responseTypeOf = typeof response?.body;
+  try {
+    const requestTypeOf = typeof request?.body;
+    const responseTypeOf = typeof response?.body;
 
-  const newRequestBody =
-    request?.body && requestTypeOf !== 'object'
-      ? { contentType: requestTypeOf, rawRequestBody: request.body }
-      : request.body;
+    const newRequestBody =
+      request?.body && requestTypeOf !== 'object'
+        ? { contentType: requestTypeOf, rawRequestBody: request.body }
+        : request.body;
 
-  const newResponseBody =
-    response?.body && responseTypeOf !== 'object'
-      ? { contentType: responseTypeOf, rawResponseBody: response.body }
-      : response.body;
+    const newResponseBody =
+      response?.body && responseTypeOf !== 'object'
+        ? { contentType: responseTypeOf, rawResponseBody: response.body }
+        : response.body;
 
-  const requestEntities = {
-    'request-body': newRequestBody,
-    'request-method': request.method,
-    'request-headers': request.headers,
-    'request-url': request.url,
-  };
+    const requestEntities = {
+      'request-body': newRequestBody,
+      'request-method': request.method,
+      'request-headers': request.headers,
+      'request-url': request.url,
+    };
 
-  const responseEntities = {
-    'response-body': newResponseBody,
-    'response-status-code': response.statusCode,
-    'response-headers': response.headers,
-  };
+    const responseEntities = {
+      'response-body': newResponseBody,
+      'response-status-code': response.statusCode,
+      'response-headers': response.headers,
+    };
 
-  logger.log({
-    level: 'http',
-    message: description,
-    meta: { keywords, services },
-    payload: {
-      request: requestEntities,
-      response: responseEntities,
-    },
-  });
+    logger.log({
+      level: 'http',
+      message: description,
+      meta: { keywords, services },
+      payload: {
+        request: requestEntities,
+        response: responseEntities,
+      },
+    });
+  } catch (error) {
+    logger.log(error);
+  }
 };
