5215d0ee5307d4acf9811923c07fb0171975a0ed
Lucas Santos <lucas.santos@pagtel.com.br>
2023-04-11 16:06:55 -0300

    chore: sets event.outcome to failure on occurrence of errors

--- src/main/factories/usecases/make-error-handler.ts
+++ src/main/factories/usecases/make-error-handler.ts
@@ -1,8 +1,13 @@
 import { ErrorHandlerProtocol as Handler } from '@/data/protocols/exception';
 import { ExErrorHandler } from '@/data/usecases/exception';
-import { logger } from '@/util';
+import { elasticAPM, logger } from '@/util';
 
 export const makeErrorHandler = (handlers: Handler[] = []) => {
-  const defaultErrorHandlers = [(error: Error) => logger.log(error)];
+  const loggerCallback = (error: Error) => logger.log(error);
+
+  const apmCallback = () =>
+    elasticAPM().getAPM()?.setTransactionOutcome('failure');
+
+  const defaultErrorHandlers = [loggerCallback, apmCallback];
   return new ExErrorHandler([...defaultErrorHandlers, ...handlers]);
 };
