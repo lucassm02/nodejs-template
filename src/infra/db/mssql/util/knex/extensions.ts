f674f9b8134d6e3c245a14da9ec06156132b0574
Lucas Santos <lucas.santos@pagtel.com.br>
2023-06-15 17:15:54 -0300

    chore: add missing property validation

--- src/infra/db/mssql/util/knex/extensions.ts
+++ src/infra/db/mssql/util/knex/extensions.ts
@@ -1,10 +1,10 @@
 import knex from 'knex';
 
-knex.QueryBuilder.extend('noLockFrom', function (from: string) {
+knex?.QueryBuilder?.extend('noLockFrom', function (from: string) {
   return this.from(this.client.raw(`${from} WITH (NOLOCK)`));
 });
 
-knex.QueryBuilder.extend('noLockInnerJoin', function (...args) {
+knex?.QueryBuilder?.extend('noLockInnerJoin', function (...args) {
   return this.innerJoin(
     this.client.raw(`${args[0]} WITH (NOLOCK)`),
     args[1],
@@ -12,7 +12,7 @@ knex.QueryBuilder.extend('noLockInnerJoin', function (...args) {
   );
 });
 
-knex.QueryBuilder.extend('noLockLeftJoin', function (...args) {
+knex?.QueryBuilder?.extend('noLockLeftJoin', function (...args) {
   return this.leftJoin(
     this.client.raw(`${args[0]} WITH (NOLOCK)`),
     args[1],
