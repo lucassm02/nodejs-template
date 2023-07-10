84ceceab4742f818bd3935eba1e4a09aa372bbe4
Lucas Santos <lucas.santos@pagtel.com.br>
2023-07-10 16:59:02 -0300

    refactor: change internal behavior of flow adapter

--- src/main/adapters/fow-adapter.ts
+++ src/main/adapters/fow-adapter.ts
@@ -1,26 +1,49 @@
-type Next = (callback?: () => void) => Promise<void>;
-export type Callback<T> = (payload: T, next: Function | Next) => void;
+import EventEmitter from 'events';
 
-export default <T>(payload: T) =>
-  (...args: Callback<T>[] | Function[]) =>
+type Next = () => void;
+export type Callback<Data> = (data: Data, next: Function | Next) => void;
+
+export default <Data extends Record<string, unknown>>(data: Data) =>
+  (...callbacks: Callback<Data>[] | Function[]) =>
   async () => {
-    const [launcher] = args
-      .reverse()
-      .reduce((callbacks: Function[], callback: Function, index) => {
-        if (index === 0) {
-          return [
-            async () =>
-              callback(payload, async (fun?: Next) => {
-                await fun?.();
-              }),
-          ];
-        }
+    const NEXT_EVENT_SYMBOL = Symbol('NEXT');
+    const RESOLVER_EVENT_SYMBOL = Symbol('RESOLVE');
 
-        const previousFunction = callbacks?.[index - 1];
+    const event = new EventEmitter();
 
-        return [...callbacks, () => callback(payload, previousFunction)];
-      }, [])
+    let resolve!: Function;
+
+    const nextFunction = () => {
+      event.emit(NEXT_EVENT_SYMBOL);
+    };
+
+    const dataClone = structuredClone(data);
+
+    const callStack = callbacks
+      .map((middleware) => () => middleware(dataClone, nextFunction))
       .reverse();
 
-    await launcher?.();
+    event.on(NEXT_EVENT_SYMBOL, async () => {
+      const handler = callStack.pop();
+
+      if (handler === undefined) {
+        event.emit(RESOLVER_EVENT_SYMBOL);
+        return;
+      }
+
+      await handler();
+    });
+
+    event.once(RESOLVER_EVENT_SYMBOL, () => {
+      resolve(undefined);
+    });
+
+    return new Promise((resolveCallback, rejectCallback) => {
+      try {
+        resolve = resolveCallback;
+        event.emit(NEXT_EVENT_SYMBOL);
+      } catch (error) {
+        rejectCallback(error);
+      }
+    });
   };
