df8b469ea8975f1c8a12f3383231703d6c1a6cf7
Guilherme-Joviniano <guilherme.sousa@pagtel.com.br>
2023-09-15 14:57:44 -0300

    refactor: change the connection to a locally sqlite instance on test environment

--- src/infra/db/mssql/util/connection/index.ts
+++ src/infra/db/mssql/util/connection/index.ts
@@ -3,17 +3,37 @@ import { DB } from '@/util/constants';
 
 import { knex } from '../knex';
 
-export const sqlConnection = knex({
-  client: DB.DIALECT,
-  connection: {
-    host: DB.HOST,
-    port: +DB.PORT,
-    user: DB.USERNAME,
-    password: DB.PASSWORD,
-    options: {
-      encrypt: false,
-      enableArithAbort: false,
-      appName: pkg.name
+const configs = {
+  default: {
+    client: DB.DIALECT,
+    connection: {
+      host: DB.HOST,
+      port: +DB.PORT,
+      user: DB.USERNAME,
+      password: DB.PASSWORD,
+      options: {
+        encrypt: false,
+        enableArithAbort: false,
+        appName: pkg.name
+      }
+    }
+  },
+  test: {
+    client: DB.DIALECT,
+    connection: {
+      host: DB.HOST,
+      port: +DB.PORT,
+      user: DB.USERNAME,
+      password: DB.PASSWORD,
+      options: {
+        encrypt: false,
+        enableArithAbort: false,
+        appName: pkg.name
+      }
     }
   }
-});
+};
+
+const config = process.env.NODE_ENV === 'test' ? configs.default : configs.test;
+
+export const sqlConnection = knex(config);
