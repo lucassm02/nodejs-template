42a055d53e60f94a86e48aca211e8b06b991e227
Samuel Goulart <samuel.a.goulart@gmail.com>
2024-01-13 23:16:20 -0300

    test: ensure getReprocessingDataController

---
+++ test/unit/presentation/controllers/reprocessing/get-reprocessing-data-controller.spec.ts
@@ -0,0 +1,80 @@
+import { GetReprocessingDataController } from '@/presentation/controllers';
+
+type SutTypes = {
+  sut: GetReprocessingDataController;
+};
+
+const makeSut = (): SutTypes => ({
+  sut: new GetReprocessingDataController()
+});
+
+describe('GetReprocessingDataController', () => {
+  const state: any = {
+    getReprocessingData: [
+      {
+        reprocessingId: 'any_reprocessing_id',
+        reprocessing: {
+          middleware: 'any_middleware',
+          tries: {},
+          data: { payload: { body: {}, headers: {}, properties: {} } },
+          state: {}
+        },
+        queue: 'any_queue',
+        createdAt: new Date('2024-01-13T23:24:13')
+      }
+    ]
+  };
+  const setState = jest.fn();
+
+  it('Should return 200 with verbose response on success', async () => {
+    const { sut } = makeSut();
+
+    const request: any = { query: { verbose: true } };
+    const result = await sut.handle(request, [state, setState]);
+
+    const reprocessingIds = ['any_reprocessing_id'];
+
+    const expected = {
+      statusCode: 200,
+      body: {
+        message: 'Reprocessamento listado com sucesso.',
+        payload: { reprocessingIds, reprocessings: state.getReprocessingData },
+        error: []
+      }
+    };
+    expect(result).toStrictEqual(expected);
+  });
+
+  it('Should return 200 with non-verbose response on success', async () => {
+    const { sut } = makeSut();
+
+    const request: any = { query: { verbose: false } };
+    const result = await sut.handle(request, [state, setState]);
+
+    const reprocessingIds = ['any_reprocessing_id'];
+
+    const payload = {
+      reprocessingIds,
+      reprocessings: [
+        {
+          reprocessingId: 'any_reprocessing_id',
+          middleware: 'any_middleware',
+          tries: {},
+          body: {},
+          headers: {},
+          createdAt: new Date('2024-01-13T23:24:13')
+        }
+      ]
+    };
+
+    const expected = {
+      statusCode: 200,
+      body: {
+        message: 'Reprocessamento listado com sucesso.',
+        payload,
+        error: []
+      }
+    };
+    expect(result).toStrictEqual(expected);
+  });
+});
