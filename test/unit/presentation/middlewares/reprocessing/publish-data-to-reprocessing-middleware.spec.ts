18f08a8a4e1f8a5c803574e30c40dae2dd5b6ceb
Samuel Goulart <samuel.a.goulart@gmail.com>
2024-01-14 00:10:49 -0300

    test: ensure PublishDataToReprocessingMiddleware

---
+++ test/unit/presentation/middlewares/reprocessing/publish-data-to-reprocessing-middleware.spec.ts
@@ -0,0 +1,63 @@
+import { ErrorHandler } from '@/domain/usecases';
+import { PublishDataToReprocessingMiddleware } from '@/presentation/middlewares';
+import { mockReprocessingModel } from '@/test/unit/domain';
+import { PublishDataToReprocessingStub } from '@/test/unit/presentation/mocks';
+import { makeErrorHandlerStub, makeServerErrorMock } from '@/test/unit/utils';
+
+type SutTypes = {
+  sut: PublishDataToReprocessingMiddleware;
+  publishDataToReprocessingStub: PublishDataToReprocessingStub;
+  errorHandler: ErrorHandler;
+};
+
+const makeSut = (): SutTypes => {
+  const publishDataToReprocessingStub = new PublishDataToReprocessingStub();
+  const errorHandler = makeErrorHandlerStub();
+  const sut = new PublishDataToReprocessingMiddleware(
+    publishDataToReprocessingStub,
+    errorHandler
+  );
+
+  return {
+    sut,
+    publishDataToReprocessingStub,
+    errorHandler
+  };
+};
+
+describe('PublishDataToReprocessingMiddleware', () => {
+  const request: any = {};
+  const state: any = {
+    getReprocessingDataByIdentifier: [mockReprocessingModel]
+  };
+  const setState = jest.fn();
+  const next = jest.fn();
+
+  it('Should call publishDataToReprocessing witch correct values', async () => {
+    const { sut, publishDataToReprocessingStub } = makeSut();
+
+    const publishSpy = jest.spyOn(publishDataToReprocessingStub, 'publish');
+
+    await sut.handle(request, [state, setState], next);
+
+    const expected = [mockReprocessingModel];
+
+    expect(publishSpy).toHaveBeenCalledTimes(1);
+    expect(publishSpy).toHaveBeenCalledWith(expected);
+  });
+
+  it('Should return 500 if a unknown error throws', async () => {
+    const { sut, publishDataToReprocessingStub, errorHandler } = makeSut();
+
+    jest
+      .spyOn(publishDataToReprocessingStub, 'publish')
+      .mockRejectedValueOnce(new Error('Unknown Error.'));
+
+    const errorHandlerSpy = jest.spyOn(errorHandler, 'handle');
+    const result = await sut.handle(request, [state, setState], next);
+
+    const expected = makeServerErrorMock();
+    expect(result).toStrictEqual(expected);
+    expect(errorHandlerSpy).toHaveBeenCalledTimes(1);
+  });
+});
--- test/unit/presentation/mocks/reprocessing/index.ts
+++ test/unit/presentation/mocks/reprocessing/index.ts
@@ -1,1 +1,2 @@
 export * from './get-reprocessing-data-stub';
+export * from './publish-data-to-reprocessing-stub';
