144189aa885c11ceea4120c3f5b2ea7107f87c6b
gjovs <00drpixelss@gmail.com>
2024-03-21 10:08:27 -0300

    refactor: remove express adapters and types and use fastify. remove redudant logic to assign the routes adapted to fastify engine.

--- src/infra/http/utils/http-server/http-server.ts
+++ src/infra/http/utils/http-server/http-server.ts
@@ -1,50 +1,44 @@
+import fastify, {
+  FastifyInstance,
+  FastifyPluginCallback,
+  RouteHandlerMethod
+} from 'fastify';
 import { readdirSync } from 'fs';
-import server, { Server } from 'http';
+import { Server } from 'http';
 import { AddressInfo } from 'net';
 import { resolve } from 'path';
-import express, {
-  Express,
-  NextFunction,
-  Request,
-  Response,
-  Router
-} from 'express';
 
-import {
-  convertCamelCaseKeysToSnakeCase,
-  convertSnakeCaseKeysToCamelCase,
-  logger
-} from '@/util';
-import { Controller, Middleware } from '@/presentation/protocols';
+import makeFlow from '@/main/adapters/flow-adapter';
+import { Middleware } from '@/presentation/protocols';
+import { SharedState } from '@/presentation/protocols/shared-state';
+import { serverError } from '@/presentation/utils';
+import { DICTIONARY, convertCamelCaseKeysToSnakeCase, logger } from '@/util';
 
 import { Route } from './route';
-import { Callback, ExpressRoute, RouteMiddleware } from './types';
-
-const SHARED_STATE_SYMBOL = Symbol('SharedState');
+import {
+  Callback,
+  Payload,
+  REPLY_KEY,
+  REQUEST_KEY,
+  RouteMiddleware,
+  STATE_KEY
+} from './types';
 
 export class HttpServer {
-  private express!: Express;
-  private server!: Server;
+  private fastify!: FastifyInstance;
   private listenerOptions!: { port: number; callback: Callback };
   private baseUrl = '';
   private addressInfo!: AddressInfo | null | string;
-  private routers: {
-    path?: string;
-    baseUrl?: string;
-    router: Router;
-    loaded: boolean;
-    hidden: boolean;
-  }[] = [];
-
   private startupCallbacks: Function[] = [];
-
   private isStarted = false;
-
   private static instance: HttpServer;
 
   constructor() {
-    this.express = express();
-    this.express.use(this.makeSharedStateInitializer());
+    this.fastify = fastify();
+  }
+
+  public use(plugin: FastifyPluginCallback, configs?: any): void {
+    this.fastify.register(plugin, configs);
   }
 
   public static getInstance(): HttpServer {
@@ -62,19 +56,26 @@ export class HttpServer {
   public listen(port: number | string, callback: () => void = () => {}) {
     if (this.isStarted) return;
     this.isStarted = true;
-
-    this.loadRoutes();
-
     this.listenerOptions = { callback, port: +port };
-    this.server = this.express.listen(port, callback);
-    this.addressInfo = this.server.address();
 
-    return this.server;
+    this.fastify.ready(() => {
+      callback();
+      this.fastify.server.listen({
+        port
+      });
+    });
+
+    this.addressInfo = this.fastify.server.address();
+
+    return this.fastify.server;
   }
 
   public getServer(): Server {
-    this.loadRoutes();
-    return server.createServer(this.express);
+    return this.fastify.server;
+  }
+
+  public async ready() {
+    await this.fastify.ready();
   }
 
   public async listenAsync(
@@ -90,13 +91,18 @@ export class HttpServer {
 
     await Promise.all(promises);
 
-    this.loadRoutes();
-
     this.listenerOptions = { callback, port: +port };
-    this.server = this.express.listen(port, callback);
-    this.addressInfo = this.server.address();
 
-    return this.server;
+    this.fastify.ready(() => {
+      callback();
+      this.fastify.server.listen({
+        port
+      });
+    });
+
+    this.addressInfo = this.fastify.server.address();
+
+    return this.fastify.server;
   }
 
   public onStart(callback: Callback, ...callbacks: Callback[]): void;
@@ -115,7 +121,7 @@ export class HttpServer {
 
   public refresh() {
     if (!this.isStarted) return;
-    this.server.close(() => {
+    this.fastify.server.close(() => {
       logger.log({ level: 'info', message: 'Refreshing server' });
     });
 
@@ -124,29 +130,9 @@ export class HttpServer {
 
   public close() {
     if (!this.isStarted) return;
-    this.server.close(() => {
+    this.fastify.server.close(() => {
       logger.log({ level: 'info', message: 'Shutting down server' });
     });
-  }
-
-  public use(
-    value: string | RouteMiddleware | ExpressRoute | Function,
-    ...middlewares: RouteMiddleware[] | ExpressRoute[]
-  ) {
-    if (typeof value === 'string') {
-      this.express.use(value, ...this.adaptMiddlewares(middlewares));
-      return;
-    }
-
-    this.express.use(...this.adaptMiddlewares([value, ...middlewares]));
-  }
-  // FIXME: NEED TO FIX STATE AS GLOBAL VARIABLE
-  // public setSharedState<T>(state: T): void {
-  //   this.express.use(this.makeSharedStateChanger(state));
-  // }
-
-  public set(setting: string, val: any) {
-    this.express.set(setting, val);
   }
 
   public setBaseUrl(url: string) {
@@ -164,44 +150,29 @@ export class HttpServer {
   public async routesDirectory(
     path: string,
     route?: Route,
-    ...middlewares: RouteMiddleware[] | ExpressRoute[] | Function[]
+    ...middlewares: RouteMiddleware[] | Function[]
   ): Promise<void>;
   public async routesDirectory(
     path: string,
     baseUrl?: string,
-    ...middlewares: RouteMiddleware[] | ExpressRoute[] | Function[]
+    ...middlewares: RouteMiddleware[] | Function[]
   ): Promise<void>;
   public async routesDirectory(
     path: string,
-    middleware?: RouteMiddleware | ExpressRoute | Function,
-    ...middlewares: RouteMiddleware[] | ExpressRoute[] | Function[]
+    middleware?: RouteMiddleware | Function,
+    ...middlewares: RouteMiddleware[] | Function[]
   ): Promise<void>;
   public async routesDirectory(
     path: string,
-    arg1?: string | RouteMiddleware | ExpressRoute | Function | Route,
-    ...args: RouteMiddleware[] | ExpressRoute[] | Function[]
+    arg1?: string | RouteMiddleware | Function | Route,
+    ...args: RouteMiddleware[] | Function[]
   ): Promise<void> {
     const extensionsToSearch = ['.TS', '.JS'];
     const ignoreIfIncludes = ['.MAP.', '.SPEC.', '.TEST.'];
 
-    const baseUrl = typeof arg1 === 'string' ? arg1 : this.baseUrl;
-
-    const middlewares =
-      typeof arg1 !== 'string' && arg1 !== undefined && !(arg1 instanceof Route)
-        ? [arg1, ...args]
-        : args;
-
     const files = readdirSync(path);
 
-    const route =
-      arg1 instanceof Route
-        ? arg1
-        : this.createRoute({ hidden: true, baseUrl, path: '' });
-
-    if (middlewares.length) {
-      const [arg1, ...args] = middlewares;
-      route.use(arg1, ...args);
-    }
+    const route = arg1 instanceof Route ? arg1 : this.createRoute();
 
     for await (const fileName of files) {
       const fileNameToUpperCase = fileName.toLocaleUpperCase();
@@ -223,135 +194,102 @@ export class HttpServer {
         setup(route);
       }
     }
-
-    this.loadRoutes();
   }
 
-  public route(options: { path?: string; baseUrl?: string }): Route;
-  public route(path?: string, baseUrl?: string): Route;
-  public route(
-    arg1?: string | { path?: string; baseUrl?: string },
-    arg2?: string
-  ) {
-    const { path, baseUrl } =
-      typeof arg1 === 'object' ? arg1 : { path: arg1, baseUrl: arg2 };
-
-    const route = this.getRoute(path, baseUrl);
-
-    if (route) return route;
-
-    return this.createRoute({ path, baseUrl });
-  }
-
-  private createRoute(options: {
-    path?: string;
-    baseUrl?: string;
-    hidden?: boolean;
-  }): Route;
-  private createRoute(path?: string, baseUrl?: string, hidden?: boolean): Route;
-  private createRoute(
-    arg1?: string | { path?: string; baseUrl?: string; hidden?: boolean },
-    arg2?: string,
-    arg3?: boolean
-  ) {
-    const { path, baseUrl, hidden } =
-      typeof arg1 === 'object'
-        ? arg1
-        : { path: arg1, baseUrl: arg2, hidden: arg3 };
-
-    const router = Router();
-    this.routers = [
-      ...this.routers,
-      { path, baseUrl, router, loaded: false, hidden: hidden || false }
-    ];
-
-    return new Route(router, this.adaptMiddlewares.bind(this));
-  }
-
-  public getRoute(path?: string, baseUrl?: string) {
-    const route = this.routers.find(
-      (route) =>
-        route.path === path && route.baseUrl === baseUrl && !route.hidden
+  private createRoute(): Route {
+    return new Route(
+      this.fastify,
+      this.adapterWithFlow.bind(this),
+      this.baseUrl
     );
-
-    if (!route) return undefined;
-
-    return new Route(route.router, this.adaptMiddlewares.bind(this));
   }
 
-  private loadRoutes() {
-    this.routers
-      .filter((router) => !router.loaded)
-      .forEach((router) => {
-        const baseUrl = router.baseUrl ?? this.baseUrl;
-        const path = router.path ?? '';
-        const url = `${baseUrl}/${path}`.replaceAll(/\/{2,}/g, '/');
-        this.express.use(url, router.router);
-        router.loaded = true;
-      });
-  }
-
-  private makeSetStateInRequest(request: Request) {
+  private makeSetStateInRequest(_state: Record<string, unknown>) {
     return <T>(state: T) => {
       for (const key in state) {
         if (typeof key === 'string' || typeof key === 'number')
-          request[SHARED_STATE_SYMBOL][key] = state[key];
+          _state[key] = state[key];
       }
     };
   }
 
-  private makeSharedStateInitializer() {
-    return (request: Request, response: Response, next: NextFunction) => {
-      request[SHARED_STATE_SYMBOL] = {};
-      next();
-    };
-  }
-
-  private makeSharedStateChanger<T>(state: T) {
-    return (request: Request, _: Response, next: NextFunction) => {
-      request[SHARED_STATE_SYMBOL] = state;
-      next();
-    };
-  }
-
-  public adapter(middleware: Middleware | Controller) {
-    return this.middlewareAdapter(middleware);
-  }
-
   private adaptMiddlewares(middlewares: RouteMiddleware[]) {
     return middlewares.map((middleware) => {
-      if (typeof middleware === 'function')
-        return (request: Request, response: Response, next: NextFunction) => {
-          const middlewareResponse = middleware(request, response, next, [
-            request[SHARED_STATE_SYMBOL],
-            this.makeSetStateInRequest(request)
-          ]);
+      return async (
+        {
+          [STATE_KEY]: state,
+          [REPLY_KEY]: reply,
+          [REQUEST_KEY]: request
+        }: Payload,
+        next: Middleware.Next
+      ) => {
+        const stateHook = <[SharedState, <T>(state: T) => void]>[
+          state,
+          this.makeSetStateInRequest(state)
+        ];
 
-          return middlewareResponse;
-        };
-      return this.middlewareAdapter(middleware);
+        const response = await (typeof middleware !== 'function'
+          ? middleware.handle(request, stateHook, next)
+          : middleware(request, reply, next, stateHook));
+
+        if (!response) return;
+
+        if (response?.headers) reply.headers(response.headers);
+
+        return reply
+          .status(response.statusCode)
+          .send(convertCamelCaseKeysToSnakeCase(response.body));
+      };
     });
   }
 
-  private middlewareAdapter(middleware: Middleware | Controller) {
-    return async (request: Request, response: Response, next: NextFunction) => {
-      request.body = convertSnakeCaseKeysToCamelCase(request.body);
-      request.params = convertSnakeCaseKeysToCamelCase(request.params);
-      request.query = convertSnakeCaseKeysToCamelCase(request.query);
+  public adapter(middleware: RouteMiddleware) {
+    return async (
+      {
+        [STATE_KEY]: state,
+        [REPLY_KEY]: reply,
+        [REQUEST_KEY]: request
+      }: Payload,
+      next: Middleware.Next
+    ) => {
+      const stateHook = <[SharedState, <T>(state: T) => void]>[
+        state,
+        this.makeSetStateInRequest(state)
+      ];
 
-      const httpResponse = await middleware.handle(
-        request,
-        [request[SHARED_STATE_SYMBOL], this.makeSetStateInRequest(request)],
-        next
-      );
+      const response = await (typeof middleware !== 'function'
+        ? middleware.handle(request, stateHook, next)
+        : middleware(request, reply, next, stateHook));
 
-      if (!httpResponse) return;
+      if (!response) return;
 
-      if (httpResponse?.headers) response.set(httpResponse.headers);
+      if (response?.headers) reply.headers(response.headers);
 
-      return response
-        .status(httpResponse?.statusCode)
-        .json(convertCamelCaseKeysToSnakeCase(httpResponse?.body));
+      return reply
+        .status(response.statusCode)
+        .send(convertCamelCaseKeysToSnakeCase(response.body));
+    };
+  }
+
+  private adapterWithFlow(middlewares: RouteMiddleware[]): RouteHandlerMethod {
+    return async (request, reply) => {
+      try {
+        await makeFlow({
+          [REQUEST_KEY]: request,
+          [STATE_KEY]: {},
+          [REPLY_KEY]: reply
+        })(...this.adaptMiddlewares(middlewares))();
+
+        if (reply.sent) return;
+
+        reply.status(502).send({
+          message: DICTIONARY.RESPONSE.MESSAGE.BAD_GATEWAY,
+          payload: {},
+          error: [{ message: 'Ocorreu um erro em nosso servidores' }]
+        });
+      } catch (error) {
+        reply.status(500).send(serverError(error));
+      }
     };
   }
 }
