a0033f78d5af84ac87aadb213d749c0e8c729910
Italo Gabriel <italo.pereira@pagtel.com.br>
2024-04-15 11:43:58 -0300

    refactor: create mock using Example mock

--- test/unit/domain/models/examples/example.ts
+++ test/unit/domain/models/examples/example.ts
@@ -20,12 +20,12 @@ export const ModelMock: Model = {
   deletedAt: 'any_deleted_at'
 };
 
-export const exampleModelMock: ExampleModel = {
-  exampleId: 1,
-  externalId: 'any_external_id',
-  value: 'any_value',
-  description: 'any_description',
+export const exampleModelMock: ExampleModel = new ExampleModel({
   createdAt: new Date('2023-01-01'),
-  updatedAt: new Date('2023-01-01'),
-  deletedAt: new Date('2023-01-01')
-};
+  description: 'any_description',
+  exampleId: 'any_example_id',
+  externalId: 'any_external_id',
+  value: 1,
+  deletedAt: new Date('2023-01-01'),
+  updatedAt: new Date('2023-01-01')
+});
