31db4eed615e766c1b3b48b851247dccde846cbd
gjovs <00drpixelss@gmail.com>
2024-04-16 15:53:48 -0300

    fix(test): ensure that all tests are working correctly after merges of main repository branch

--- test/unit/data/protocols/db/example/create-example-repository-stub.ts
+++ test/unit/data/protocols/db/example/create-example-repository-stub.ts
@@ -1,17 +1,10 @@
 import { CreateExampleRepository } from '@/data/protocols/db';
+import { exampleModelMock } from '@/test/unit/domain/models';
 
 export class CreateExampleRepositoryStub implements CreateExampleRepository {
   create(): CreateExampleRepository.Result {
     return Promise.resolve({
-      record: {
-        exampleId: 1,
-        externalId: 'external_id',
-        value: 'value',
-        description: 'description',
-        createdAt: new Date('2023-01-01'),
-        updatedAt: new Date('2023-01-01'),
-        deletedAt: new Date('2023-01-01')
-      },
+      record: exampleModelMock,
       transaction: { commit: async () => {}, rollback: async () => {} }
     });
   }
