d4bc4c6ad83dffb38e0eef36694339cb855452fd
gjovs <00drpixelss@gmail.com>
2024-10-18 11:25:50 -0300

    feat: Add WebSocket server and factory to http server utils

---
+++ src/infra/http/utils/websocket-server/websocket-server.ts
@@ -0,0 +1,201 @@
+import type { Server as HttpServer } from 'http';
+import { Server as SocketServer, type Socket } from 'socket.io';
+
+import makeFlow from '@/main/adapters/flow-adapter';
+import type { Middleware, SharedState } from '@/presentation/protocols';
+import { convertCamelCaseKeysToSnakeCase, logger } from '@/util';
+
+import { SocketHandler } from '../http-server/socket-handler';
+import {
+  EVENT_KEY,
+  REQUEST_KEY,
+  SOCKET_KEY,
+  STATE_KEY,
+  type Payload,
+  type RouteMiddlewareSocket,
+  type WebSocketCallbackMetadata
+} from '../http-server/types';
+
+export type WebSocketServerOptions = {
+  path: string;
+  enabled: boolean;
+  cors: {
+    origin: string | string[];
+    methods: string[];
+  };
+};
+
+export class WebSocketServer {
+  private ws!: SocketServer;
+  private static instance: WebSocketServer;
+
+  private constructor(
+    private readonly httpServer: HttpServer,
+    private readonly options: WebSocketServerOptions
+  ) {
+    this.ws = new SocketServer(this.httpServer, {
+      cors: this.options.cors,
+      path: this.options.path,
+      transports: ['websocket', 'polling']
+    });
+  }
+
+  static getInstance(httpServer: HttpServer, options: WebSocketServerOptions) {
+    if (!this.instance) {
+      this.instance = new WebSocketServer(httpServer, options);
+    }
+
+    return this.instance;
+  }
+
+  private makeSetStateInRequest(source: Record<string, unknown>) {
+    return <T>(state: T) => {
+      for (const key in state) {
+        if (typeof key === 'string' || typeof key === 'number')
+          source[key] = state[key];
+      }
+    };
+  }
+
+  public adapter(middleware: RouteMiddlewareSocket, method: string) {
+    return async (
+      {
+        [STATE_KEY]: state,
+        [SOCKET_KEY]: socket,
+        [REQUEST_KEY]: request
+      }: Payload,
+      next: Middleware.Next
+    ) => {
+      const stateHook = <[SharedState, <T>(state: T) => void]>[
+        state,
+        this.makeSetStateInRequest(state)
+      ];
+
+      const response = await (typeof middleware !== 'function'
+        ? middleware.handle(request, stateHook, next)
+        : middleware(socket, request, next, stateHook));
+
+      if (!response) return;
+
+      const { statusCode: httpStatusCode, ...data } = response;
+
+      const { options = null } = data;
+
+      socket.emit(
+        method,
+        convertCamelCaseKeysToSnakeCase({
+          httpStatusCode,
+          ...data
+        })
+      );
+
+      if (options?.close) socket.disconnect();
+    };
+  }
+
+  private adaptMiddlewaresWebSocket(
+    middlewares: RouteMiddlewareSocket[],
+    method: string
+  ) {
+    return middlewares.map((middleware) => {
+      return async (
+        {
+          [STATE_KEY]: state,
+          [SOCKET_KEY]: socket,
+          [REQUEST_KEY]: request
+        }: Payload,
+        next: Middleware.Next
+      ) => {
+        const stateHook = <[SharedState, <T>(state: T) => void]>[
+          state,
+          this.makeSetStateInRequest(state)
+        ];
+
+        const response = await (typeof middleware !== 'function'
+          ? middleware.handle(request, stateHook, next)
+          : middleware(socket, request, next, stateHook));
+
+        if (!response) return;
+
+        const { statusCode: httpStatusCode, ...data } = response;
+
+        const { options = null } = data;
+
+        socket.emit(
+          method,
+          convertCamelCaseKeysToSnakeCase({
+            httpStatusCode,
+            ...data
+          })
+        );
+
+        if (options?.close) socket.disconnect();
+      };
+    });
+  }
+
+  private adapterWebsocketWithFlow(
+    handlers: Map<string, Nullable<WebSocketCallbackMetadata>>
+  ): (socket: Socket) => Promise<void> {
+    return async (socket) => {
+      try {
+        for (const [_, metadata] of handlers) {
+          if (!metadata) continue;
+
+          const { middlewares, method } = metadata;
+
+          socket.on(method, async (request) => {
+            try {
+              if (!request[STATE_KEY]) {
+                request[STATE_KEY] = {};
+              }
+              if (!request[EVENT_KEY]) {
+                request[EVENT_KEY] = method;
+              }
+
+              await makeFlow({
+                [REQUEST_KEY]: request,
+                [STATE_KEY]: request[STATE_KEY],
+                [SOCKET_KEY]: socket
+              })(...this.adaptMiddlewaresWebSocket(middlewares, method))();
+            } catch (error) {
+              socket.disconnect();
+              logger.log({
+                level: 'error',
+                message:
+                  'An error occurred while processing the websocket connection',
+                error
+              });
+            }
+          });
+        }
+      } catch (error) {
+        logger.log({
+          level: 'error',
+          message:
+            'An error occurred while processing the websocket connection',
+          error
+        });
+      }
+    };
+  }
+
+  public bootstrap() {
+    this.execute();
+  }
+
+  private execute() {
+    if (!this.options.enabled) {
+      logger.log({
+        level: 'warn',
+        message: '[CAUTION] Websocket is disabled and will not be initialized!'
+      });
+      return;
+    }
+
+    this.ws.on(
+      'connection',
+      this.adapterWebsocketWithFlow(SocketHandler.getSocketHandlersCallbacks())
+    );
+  }
+}
